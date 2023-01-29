531bea3716c082bc7ab363e0b178e0cb23f22623
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-29 12:00:38 +0100

    Initial commit, no router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { ContentContainerComponent } from './content-container/content-container.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatButtonModule} from "@angular/material/button";
+import {RouterOutlet} from "@angular/router";
 
+// JSON - JavaScript Object Notation
+// {
+//   "nazwaPola": "wartosc"
+// }
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    NavigatorComponent,
+    ContentContainerComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        RouterOutlet
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })

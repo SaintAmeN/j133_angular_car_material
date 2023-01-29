2a39d30ff8afa4ee34f3186c0c464dd4638be6e3
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-29 12:48:50 +0100

    Zmiany od przerwy

---
+++ src/app/model/car.ts
@@ -0,0 +1,17 @@
+export enum CarManufacturer {
+  BMW,
+  MERCEDES,
+  LEXUS,
+  VW,
+  MAZDA,
+  OPEL,
+  FORD,
+}
+
+export type Car = {
+  id: number,
+  name: string,
+  manufacturer: CarManufacturer,
+  model: string,
+  engineCapacity: number, // pojemność silnika, np. 1.6
+}
